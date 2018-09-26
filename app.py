938add2875eefb96a1ff1ae3aec536c07c14b623
sdxy36 <0582345@163.com>
2018-09-27 05:51:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    retrn 'hello %s' % (socket.gethostname())
+    retrn 'hello %s, version 1.0' % (socket.gethostname())
 
 
 if __name__ == '__main__':
